9fb96a108f796c64fdf59f6cd5060d2c68e323a7
jjingofarouk <jjingofaroukk@gmail.com>
2025-01-26 05:34:06 +0300

    Rendered only the Symptom component and updated styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import './App.css'; // Import the CSS file
 import Symptom from './components/symptomchecker/Symptom'; // Import the SymptomChecker component
 
 function App() {
   return (
     <div className="App">
-      <h1>Symptom Checker</h1>
-      <p>Enter your symptoms below to find possible conditions and guidance.</p>
-      <div className="symptom-container">
-        <Symptom /> {/* Render only the SymptomChecker component */}
-      </div>
+      <Symptom /> {/* Render only the SymptomChecker component */}
     </div>
   );
 }
